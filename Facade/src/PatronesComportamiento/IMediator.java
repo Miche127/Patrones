e3299faf800bebcf2501e1d86e5e63c12cb3d992
Michel Mendez Mendoza <20161271@itoaxaca.edu.mx>
2025-05-21 09:39:51 -0600

    Delete Facade directory

--- Facade/src/PatronesComportamiento/IMediator.java
+++
@@ -1,9 +0,0 @@
-package PatronesComportamiento;
-
-import javax.swing.*;
-
-public interface IMediator {
-    void exportarPDF(JLabel lblFactura, JLabel lblFechaFactura, JLabel lblNombreCliente,
-                     JLabel lblAppaterno, JLabel lblApmaterno,
-                     JTable tablaProductos, JLabel lblIVA, JLabel lblTotal);
-}
